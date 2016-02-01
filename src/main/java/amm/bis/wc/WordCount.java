db65a79c8d5fd0d59a3a5c106e2d325c46b2b276
amanas <andresmanasmanas@gmail.com>
2016-02-01 12:48:50 +0100

    draft

---
+++ src/main/java/amm/bis/wc/WordCount.java
@@ -0,0 +1,50 @@
+package amm.bis.wc;
+
+import static java.lang.System.exit;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class WordCount extends Configured implements Tool {
+
+    private static Configuration conf;
+
+    @Override
+    public int run(String[] args) throws Exception {
+
+        Job job = Job.getInstance(conf, "bis-wc-job");
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
+
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        conf = new Configuration();
+
+        exit(ToolRunner.run(conf, new WordCount(), args));
+    }
+
+}
---
+++ src/test/java/amm/bis/wc/WordCountTest.java
@@ -0,0 +1,79 @@
+package amm.bis.wc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WordCountTest {
+
+    // Specification of Mapper
+    MapDriver<LongWritable, Text, Text, IntWritable> mapDriver;
+
+    // Specification of Reduce
+    ReduceDriver<Text, IntWritable, Text, IntWritable> reduceDriver;
+
+    // Specification of MapReduce program
+    MapReduceDriver<LongWritable, Text, Text, IntWritable, Text, IntWritable> mapReduceDriver;
+
+    @Before
+    public void setUp() {
+        Map mapper = new Map();
+        Reduce reducer = new Reduce();
+        // Setup Mapper
+        mapDriver = MapDriver.newMapDriver(mapper);
+        // Setup Reduce
+        reduceDriver = ReduceDriver.newReduceDriver(reducer);
+        // Setup MapReduce job
+        mapReduceDriver = MapReduceDriver.newMapReduceDriver(mapper, reducer);
+    }
+
+    @Test
+    public void testMapper() {
+        // Test Mapper with this input
+        mapDriver.withInput(new LongWritable(), new Text("hello hola hello hola hello"));
+        // Expect this output
+        mapDriver.withOutput(new Text("hello"), new IntWritable(1));
+        mapDriver.withOutput(new Text("hola"), new IntWritable(1));
+        mapDriver.withOutput(new Text("hello"), new IntWritable(1));
+        mapDriver.withOutput(new Text("hola"), new IntWritable(1));
+        mapDriver.withOutput(new Text("hello"), new IntWritable(1));
+
+        try {
+            // Run Map test with above input and ouput
+            mapDriver.runTest();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testReducer() {
+        List<IntWritable> helloValues = new ArrayList<IntWritable>();
+        helloValues.add(new IntWritable(1));
+        helloValues.add(new IntWritable(2));
+        List<IntWritable> holaValues = new ArrayList<IntWritable>();
+        holaValues.add(new IntWritable(1));
+        holaValues.add(new IntWritable(1));
+        // Run Reduce with this input
+        reduceDriver.withInput(new Text("hello"), helloValues);
+        reduceDriver.withInput(new Text("hola"), holaValues);
+        // Expect this output
+        reduceDriver.withOutput(new Text("hello"), new IntWritable(3));
+        reduceDriver.withOutput(new Text("hola"), new IntWritable(2));
+        try {
+            // Run Reduce test with above input and ouput
+            reduceDriver.runTest();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
